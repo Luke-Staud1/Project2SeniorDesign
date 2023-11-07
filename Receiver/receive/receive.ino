ab534fcecedb45071baf4300fc6e7cbea52aa1bc
Luke-Staud1 <112638106+Luke-Staud1@users.noreply.github.com>
2023-11-07 16:55:39 -0600

    it works oml

--- Receiver/receive/receive.ino
+++ Receiver/receive/receive.ino
@@ -2,11 +2,15 @@ const int RECIVE_PIN = 6;
 const int MESSAGE_SIZE = 9;
 const int MESSAGE_TIMEOUT = 1000; // timeout in miliseconds
 bool *fifo = new bool[MESSAGE_SIZE];
+int bytesRecived = 0;
+int errorsFound = 0;
 char match = 0x07;
 char key = 0x00;
 int bitTracker = 0;
 bool timeoutHit = false;
 long int milisLastUpdate = 0;
+long int frequencyTime = 0;
+long long int averageFreqTime = 0;
 int threshHold = 100;
 
 void setup()
@@ -22,15 +26,19 @@ void loop()
 
     if (!(digitalRead(RECIVE_PIN)))
     {
-        
+        frequencyTime = micros();
         readBit();
         timeoutHit = false;
         bitTracker++;
         milisLastUpdate = millis();
+        averageFreqTime = averageFreqTime + (micros() - frequencyTime);
+        
     }
     if (bitTracker == MESSAGE_SIZE)
     {
+        bytesRecived++;
         bitTracker = 0;
+//        printBinary(fifo, 9);
         if (verifyByte(fifo))
         {
             key = convetToAscii();
@@ -39,6 +47,7 @@ void loop()
         }
         else
         {
+            errorsFound++;
             fifoFlush();
             Serial.println("Error in transmission detected");
         }
@@ -53,8 +62,20 @@ void loop()
         if (!timeoutHit)
         {
             timeoutHit = true;
+            double errorRate = float(errorsFound * 8)/float(bytesRecived * 9);
+            double averageTime =  (float(averageFreqTime)/float(bytesRecived * 8))/1000;
+            
+            float frequency = 1000/averageTime;
+            Serial.print("frequency(Hz) of tranmission: ");
+            Serial.println(frequency);            
+            Serial.print("error rate of last segment (percent): ");
+            Serial.println(errorRate);
             Serial.print("Timeout reached no update in(ms): ");
             Serial.println(millis() - milisLastUpdate);
+            milisLastUpdate = 0;
+            averageFreqTime = 0;
+            errorsFound = 0;
+            bytesRecived = 0;
             milisLastUpdate = millis();
         }
     }
@@ -86,11 +107,6 @@ char convetToAscii()
     {
         dataSegment[i] = fifo[i];
     }
-    // for (int i = 0; i < MESSAGE_SIZE - 1; i++)
-    // {
-    //     flipedData[i] = dataSegment[MESSAGE_SIZE - 2 - i];
-    // }
-    // printBinary(flipedData, MESSAGE_SIZE - 1);
 
     for (int i = 0; i < MESSAGE_SIZE - 1; i++)
     {
@@ -139,7 +155,7 @@ bool verifyByte(bool byteMessage[])
 
 bool readBit()
 {
-    delayMicroseconds(200);
+    delayMicroseconds(220);
     fifoPush(digitalRead(RECIVE_PIN));
-    delayMicroseconds(200); // we can lower this time to account for how long fifo push will take to run
+    delayMicroseconds(300); // we can lower this time to account for how long fifo push will take to run
 }
